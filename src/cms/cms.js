c1f084f6a29909ca1791421d09dc6fe1385e960b
mauriciabad <hello@mauriciabad.com>
2020-05-17 16:58:29 +0200

    Set lang to Sapnish

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,14 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
+import { es } from 'netlify-cms-locales'
+
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+
+CMS.registerLocale('es', es);
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
