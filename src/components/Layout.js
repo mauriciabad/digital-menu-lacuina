38ebe1e96f733fd20c939fdcd95a4969da9b7d71
mauriciabad <hello@mauriciabad.com>
2020-05-17 23:41:07 +0200

    Add favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,15 @@ import './all.scss'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
 
+// import logo from '../img/logo.svg'
+import faviconApple from '../img/favicon/apple-touch-icon.png'
+import favicon32 from '../img/favicon/favicon-32x32.png'
+import favicon16 from '../img/favicon/favicon-16x16.png'
+import faviconManifest from 'raw-loader!../img/favicon/site.webmanifest'
+import faviconSafari from '../img/favicon/safari-pinned-tab.svg'
+import favicon from '../img/favicon/favicon.ico'
+import faviconBrowserconfig from 'raw-loader!../img/favicon/browserconfig.xml'
+
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
   return (
@@ -12,38 +21,24 @@ const TemplateWrapper = ({ children }) => {
         <title>{title}</title>
         <meta name="description" content={description} />
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
-
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
-        <meta name="theme-color" content="#fff" />
-
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
-        />
+        <meta property="og:image" content={`${withPrefix('/')}img/og-image.jpg`} />
+
+
+
+
+        <link rel="apple-touch-icon" sizes="180x180" href={faviconApple} />
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+        <link rel="manifest" href={faviconManifest} />
+        <link rel="mask-icon" href={faviconSafari} color="#51c1b1" />
+        <link rel="shortcut icon" href={favicon} />
+        <meta name="msapplication-TileColor" content="#51c1b1" />
+        <meta name="msapplication-config" content={faviconBrowserconfig} />
+        <meta name="theme-color" content="#51c1b1" />
+
       </Helmet>
       <div>{children}</div>
     </div>
