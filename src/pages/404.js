5942973f8d1284e97a311d9b7a280636375164c6
mauriciabad <hello@mauriciabad.com>
2020-05-17 19:07:08 +0200

    Empty 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,9 @@
 import React from 'react'
 
-import Layout from '../components/Layout'
-import MenuRoll from '../components/MenuRoll'
-
 export default class NotFoundPage extends React.Component {
   render() {
     return (
-      <Layout>
-        <div className="content">
-          <MenuRoll />
-        </div>
-      </Layout>
+      <div></div>
     )
   }
 }
