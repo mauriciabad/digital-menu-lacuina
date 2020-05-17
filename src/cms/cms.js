382806c9ee53cbc92d64ff7b396d8aee23d4f36f
mauriciabad <hello@mauriciabad.com>
2020-05-18 00:12:03 +0200

    Remove unused files

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,14 +1,9 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import { es } from 'netlify-cms-locales'
 
 import MenuPostPreview from './preview-templates/MenuPostPreview'
 
 CMS.registerLocale('es', es);
-
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('menu', MenuPostPreview)
