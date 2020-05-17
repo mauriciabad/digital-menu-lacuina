5c059c85b6c2431dc0b0f10f0cab9d4c77b538e0
mauriciabad <hello@mauriciabad.com>
2020-05-17 18:06:41 +0200

    Add MenuRoll

---
+++ src/components/MenuRoll.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+
+class MenuRoll extends React.Component {
+  render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
+    return (
+      <div className="menu-link__list">
+        {posts &&
+          posts.map(({ node: post }) => (
+            <Link
+              className="menu-link"
+              to={post.fields.slug}
+              >
+              <div>
+                <PreviewCompatibleImage
+                  imageInfo={{
+                    image: post.frontmatter.langimage || '/img/menu.png',
+                    alt: '',
+                  }}
+                />
+                <span>{post.frontmatter.title}</span>  
+              </div>
+            </Link>
+          ))}
+      </div>
+    )
+  }
+}
+
+MenuRoll.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query MenuRollQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "menu-post" } } }
+        ) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                templateKey
+                date(formatString: "DD MMMM YYYY")
+                langimage
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => <MenuRoll data={data} count={count} />}
+  />
+)
